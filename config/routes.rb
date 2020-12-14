6bb01027b86dc363eeb1351b47b100434fff9437
mthrsc <abbadonlefleau@hotmail.com>
2020-12-14 20:28:11 +0000

    xxx

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  get 'cart/index'
+  get "cart/index"
   resources :books
   root "static_pages#home"
 
   get "/home" => "static_pages#home"
   get "/about" => "static_pages#about"
 
-  get "/login", to: "user#login"
-  get "/logout", to: "user#logout"
+  get "/login" => "user#login"
+  get "/logout" => "user#logout"
+
+  get "/cart/clear" => "cart#clear"
+  get "/cart" => "cart#index"
+  get "/cart/:id" => "cart#add"
+  get "/cart/remove/:id" => "cart#remove"
+  get "/cart/increase/:id" => "cart#increase"
+  get "/cart/decrease/:id" => "cart#decrease"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
