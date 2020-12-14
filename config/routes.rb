abddc4566eb4afab8891941e499a5dd9616a3bf8
mthrsc <abbadonlefleau@hotmail.com>
2020-12-14 16:06:38 +0000

    xxx

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'cart/index'
   resources :books
   root "static_pages#home"
 
   get "/home" => "static_pages#home"
   get "/about" => "static_pages#about"
+
+  get "/login", to: "user#login"
+  get "/logout", to: "user#logout"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
