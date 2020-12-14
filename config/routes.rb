0c2adb36be97654ef057bee91377b5907aa9672f
mthrsc <abbadonlefleau@hotmail.com>
2020-12-14 14:06:39 +0000

    xxx

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
+  root "static_pages#home"
 
-  get "/" => "static_pages#home"
+  get "/home" => "static_pages#home"
   get "/about" => "static_pages#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
