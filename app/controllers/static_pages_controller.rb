3c46f1332aacde43f6ed65f83337d42e3e26d991
mthrsc <abbadonlefleau@hotmail.com>
2020-12-16 15:37:48 +0000

    tuto9

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    temp = Book.order("created_at DESC")
+    @books = Array.new(5)
+    i = 0
+    loop do
+      @books[i] = temp[i]
+      i += 1
+      if i == @books.length()
+        break
+      end
+    end
   end
 
   def about
